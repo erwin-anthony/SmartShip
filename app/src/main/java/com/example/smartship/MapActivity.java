d71215a7cd99b750c119e5ac40879b79e4ac2b7e
erwin_anthony <anthony.20140617@gmail.com>
2021-05-14 15:49:24 +0800

    add:complete the MQTT

--- app/src/main/java/com/example/smartship/MapActivity.java
+++ app/src/main/java/com/example/smartship/MapActivity.java
@@ -33,10 +33,19 @@ import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class MapActivity extends AppCompatActivity {
@@ -49,6 +58,20 @@ public class MapActivity extends AppCompatActivity {
     boolean isFirstLocate =true;
     private RemoteView controlDirection;
     private View btn4;
+    private Object mqttManager;
+    private String host = "tcp://a1jdH1fiX5K.iot-as-mqtt.cn-shanghai.aliyuncs.com:1883";
+    private String userName = "smart_ship&a1jdH1fiX5K";
+    private String passWord = "ad9d4785a55cb288122d01192aa43f7a431fb008";
+    private String mqtt_id = "smart_ship|securemode=3,signmethod=hmacsha1,timestamp=789|"; //定义成自己的QQ号  切记！不然会掉线！！！
+    private String mqtt_sub_topic = "609510594"; //为了保证你不受到别人的消息  哈哈
+    private String mqtt_pub_topic = "/a1jdH1fiX5K/${deviceName}/user/ship_control"; //为了保证你不受到别人的消息  哈哈  自己QQ好后面加 _PC
+    private ScheduledExecutorService scheduler;
+    private TextView text_test;
+    private MqttClient client;
+    private MqttConnectOptions options;
+    private Handler handler;
+
+    @SuppressLint("HandlerLeak")
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +81,42 @@ public class MapActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_map);
+
+        Mqtt_init();
+        startReconnect();
+
+        handler = new Handler() {
+            @SuppressLint("SetTextI18n")
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what){
+                    case 1: //开机校验更新回传
+                        break;
+                    case 2:  // 反馈回传
+
+                        break;
+                    case 3:  //MQTT 收到消息回传   UTF8Buffer msg=new UTF8Buffer(object.toString());
+                        Toast.makeText(MapActivity.this,msg.obj.toString() ,Toast.LENGTH_SHORT).show();
+                        text_test.setText(msg.obj.toString());
+                        break;
+                    case 30:  //连接失败
+                        Toast.makeText(MapActivity.this,"连接失败" ,Toast.LENGTH_SHORT).show();
+                        break;
+                    case 31:   //连接成功
+                        Toast.makeText(MapActivity.this,"连接成功" ,Toast.LENGTH_SHORT).show();
+                        try {
+                            client.subscribe(mqtt_sub_topic,1);
+                        } catch (MqttException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+
+
 
         locationInfo = findViewById(R.id.locationInfo);
         mLocationClient = new LocationClient(getApplicationContext());
@@ -275,13 +334,98 @@ public class MapActivity extends AppCompatActivity {
 
     }
 
+    private void Mqtt_init()
+    {
+        try {
+            //host为主机名，test为clientid即连接MQTT的客户端ID，一般以客户端唯一标识符表示，MemoryPersistence设置clientid的保存形式，默认为以内存保存
+            client = new MqttClient(host, mqtt_id,
+                    new MemoryPersistence());
+            //MQTT的连接设置
+            options = new MqttConnectOptions();
+            //设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接
+            options.setCleanSession(false);
+            //设置连接的用户名
+            options.setUserName(userName);
+            //设置连接的密码
+            options.setPassword(passWord.toCharArray());
+            // 设置超时时间 单位为秒
+            options.setConnectionTimeout(70);
+            // 设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送个消息判断客户端是否在线，但这个方法并没有重连的机制
+            options.setKeepAliveInterval(100);
+            //设置回调
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable cause) {
+                    //连接丢失后，一般在这里面进行重连
+                    System.out.println("connectionLost----------");
+                    //startReconnect();
+                }
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    //publish后会执行到这里
+                    System.out.println("deliveryComplete---------"
+                            + token.isComplete());
+                }
+                @Override
+                public void messageArrived(String topicName, MqttMessage message)
+                        throws Exception {
+                    //subscribe后得到的消息会执行到这里面
+                    System.out.println("messageArrived----------");
+                    Message msg = new Message();
+                    msg.what = 3;   //收到消息标志位
+                    msg.obj = topicName + "---" + message.toString();
+                    handler.sendMessage(msg);    // hander 回传
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void Mqtt_connect() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if(!(client.isConnected()) )  //如果还未连接
+                    {
+                        client.connect(options);
+                        Message msg = new Message();
+                        msg.what = 31;
+                        handler.sendMessage(msg);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Message msg = new Message();
+                    msg.what = 30;
+                    handler.sendMessage(msg);
+                }
+            }
+        }).start();
+    }
+    private void startReconnect() {
+        scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                if (!client.isConnected()) {
+                    Mqtt_connect();
+                }
+            }
+        }, 0 * 1000, 10 * 1000, TimeUnit.MILLISECONDS);
+    }
+    private void publishmessageplus(String topic,String message2)
+    {
+        if (client == null || !client.isConnected()) {
+            return;
+        }
+        MqttMessage message = new MqttMessage();
+        message.setPayload(message2.getBytes());
+        try {
+            client.publish(topic,message);
+        } catch (MqttException e) {
 
-
-    public  void getGdMapUri(){
-        Uri uri=Uri.parse("https://www.aotu.fun/qiq");
-        Intent intent= new Intent(Intent.ACTION_VIEW, uri);
-        intent.setData(uri);
-        startActivity(intent);
+            e.printStackTrace();
+        }
     }
 
 }
